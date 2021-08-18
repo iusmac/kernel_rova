349512c385f34ae43d5ddd8d20abbf4024d0e829
Reinazhard <reinazhard@gmail.com>
2021-08-18 17:02:08 +0700

    Revert "sched: Resolve sched_feat() at compile time to improve code optimization"
    - Seems made the UI feels heavier
    
    This reverts commit 2200654ed4048511953ba2fbaba9077ca7b025ba.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -122,6 +122,19 @@ void update_rq_clock(struct rq *rq)
 }
 
 /*
+ * Debugging: various feature bits
+ */
+
+#define SCHED_FEAT(name, enabled)	\
+	(1UL << __SCHED_FEAT_##name) * enabled |
+
+const_debug unsigned int sysctl_sched_features =
+#include "features.h"
+	0;
+
+#undef SCHED_FEAT
+
+/*
  * Number of tasks to iterate in a single balance run.
  * Limited because this is done with IRQs disabled.
  */
@@ -2554,7 +2567,6 @@ static int ttwu_remote(struct task_struct *p, int wake_flags)
 }
 
 #ifdef CONFIG_SMP
-#if SCHED_FEAT_TTWU_QUEUE
 void sched_ttwu_pending(void)
 {
 	struct rq *rq = this_rq();
@@ -2584,7 +2596,6 @@ void sched_ttwu_pending(void)
 	rq_unpin_lock(rq, &rf);
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
 }
-#endif
 
 void scheduler_ipi(void)
 {
@@ -2597,14 +2608,9 @@ void scheduler_ipi(void)
 	 */
 	preempt_fold_need_resched();
 
-#if SCHED_FEAT_TTWU_QUEUE
 	if (llist_empty(&this_rq()->wake_list) && !got_nohz_idle_kick() &&
 							!got_boost_kick())
 		return;
-#else
-	if (!got_nohz_idle_kick())
-		return;
-#endif
 
 	if (got_boost_kick()) {
 		struct rq *rq = cpu_rq(cpu);
@@ -2640,7 +2646,6 @@ void scheduler_ipi(void)
 	irq_exit();
 }
 
-#if SCHED_FEAT_TTWU_QUEUE
 static void ttwu_queue_remote(struct task_struct *p, int cpu, int wake_flags)
 {
 	struct rq *rq = cpu_rq(cpu);
@@ -2654,7 +2659,6 @@ static void ttwu_queue_remote(struct task_struct *p, int cpu, int wake_flags)
 			trace_sched_wake_idle_without_ipi(cpu);
 	}
 }
-#endif
 
 void wake_up_if_idle(int cpu)
 {
@@ -2695,13 +2699,11 @@ static void ttwu_queue(struct task_struct *p, int cpu, int wake_flags)
 	struct rq_flags rf;
 
 #if defined(CONFIG_SMP)
-#if SCHED_FEAT_TTWU_QUEUE
-	if (!idle_cpu(cpu) && !cpus_share_cache(smp_processor_id(), cpu)) {
-		sched_clock_cpu(cpu); /* Sync clocks across CPUs */
+	if (sched_feat(TTWU_QUEUE) && !cpus_share_cache(smp_processor_id(), cpu)) {
+		sched_clock_cpu(cpu); /* sync clocks x-cpu */
 		ttwu_queue_remote(p, cpu, wake_flags);
 		return;
 	}
-#endif
 #endif
 
 	raw_spin_lock(&rq->lock);
@@ -4326,9 +4328,8 @@ static void __sched notrace __schedule(bool preempt)
 
 	schedule_debug(prev);
 
-#if SCHED_FEAT_HRTICK
-	hrtick_clear(rq);
-#endif
+	if (sched_feat(HRTICK))
+		hrtick_clear(rq);
 
 	local_irq_disable();
 	rcu_note_context_switch();
@@ -4857,10 +4858,8 @@ int idle_cpu(int cpu)
 		return 0;
 
 #ifdef CONFIG_SMP
-#if SCHED_FEAT_TTWU_QUEUE
 	if (!llist_empty(&rq->wake_list))
 		return 0;
-#endif
 #endif
 
 	return 1;
