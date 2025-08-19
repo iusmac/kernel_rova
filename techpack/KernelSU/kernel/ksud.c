be8846ae7a29128829d7b11da7ac520f80dc80ce
iusmac <iusico.maxim@libero.it>
2025-08-19 10:16:38 +0200

    Merge commit 'db7d73c202528ad8b680c7c44454f733e5b0384b' as 'techpack/KernelSU/'

--- techpack/KernelSU/kernel/ksud.c
+++ techpack/KernelSU/kernel/ksud.c
@@ -58,11 +58,11 @@ static void stop_input_hook();
 static struct work_struct stop_vfs_read_work;
 static struct work_struct stop_execve_hook_work;
 static struct work_struct stop_input_hook_work;
-#endif
-
+#else
 bool ksu_vfs_read_hook __read_mostly = true;
 bool ksu_execveat_hook __read_mostly = true;
 bool ksu_input_hook __read_mostly = true;
+#endif
 
 #ifdef CONFIG_KSU_SUSFS_SUS_SU
 bool susfs_is_sus_su_ready = false;
@@ -491,10 +491,12 @@ __maybe_unused int ksu_handle_execve_ksud(const char __user *filename_user,
 	struct filename filename_in, *filename_p;
 	char path[32];
 
+#ifndef CONFIG_KSU_KPROBES_HOOK
 	// return early if disabled.
 	if (!ksu_execveat_hook) {
 		return 0;
 	}
+#endif
 
 	if (!filename_user)
 		return 0;
