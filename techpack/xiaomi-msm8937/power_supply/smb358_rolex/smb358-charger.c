ff5a82179eb3ee9e73246d2e3e7432ae61fba5c9
iusmac <iusico.maxim@libero.it>
2025-10-07 15:24:17 +0200

    mi8937: ps: smb358: Report 0mA instead of fake current_now when charging
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- techpack/xiaomi-msm8937/power_supply/smb358_rolex/smb358-charger.c
+++ techpack/xiaomi-msm8937/power_supply/smb358_rolex/smb358-charger.c
@@ -1265,9 +1265,9 @@ static int get_prop_current_now(struct smb358_charger *chip)
 			switch (ret.intval) {
 				case POWER_SUPPLY_TYPE_USB_CDP:
 				case POWER_SUPPLY_TYPE_USB_DCP:
-					return -500000; // Fast charging
+					return 0; // Fast charging
 				case POWER_SUPPLY_TYPE_USB:
-					return 30000; // Slow charging
+					return 0; // Slow charging
 				default:
 					return 500000; // Discharging
 			}
